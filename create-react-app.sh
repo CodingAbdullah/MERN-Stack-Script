c722070b94f850974230662e4b4a24744fbf3c78
Abdul <abdullahmd326@gmail.com>
2021-06-27 00:32:31 -0400

    Refined changes to script

--- create-react-app.sh
+++ create-react-app.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
-
+# sh create-react-app.sh (laptop-name)
 NAME="$1"
 cd "/Users/${NAME}/Desktop/"
 
-mkdir mern_stack_folder # Build as you like, change the name of the folder to that which you desire. This is a default name I have selected.
+ # Build as you like, change the name of the folder to that which you desire. This is a default name I have selected.
+mkdir mern_stack_folder
+
 cd mern_stack_folder
 
-# X ---- Make sure you have node installed and the npm registry within your device! ---- X
-npx create-react-app .  
+# Build two folders separating work related to front-end and back-end 
+mkdir front-end back-end
+cd front-end
 
+# X ---- Make sure you have node installed and the npm registry within your device! ---- X
+npx create-react-app . 
+
+# Up one level on the directory tree
+cd ../ 
+
+cd back-end
+npm init
+
+# Barebone modules installed by default, feel free to add your custom dependencies as you build your projects
+npm install axios bcrypt cors dotenv express jsonwebtoken mongoose
